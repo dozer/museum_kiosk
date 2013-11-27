2a5a12849b87e5141b2ea41306b8da7f6c977478
jakraabit1 <jakraabit1@gmail.com>
2013-11-27 17:50:27 -0500

    added corresponding comments for previous commit

--- MuseumInformationSystem/src/model/FloorPlan.java
+++ MuseumInformationSystem/src/model/FloorPlan.java
@@ -6,10 +6,10 @@ import enums.Structure;
  * to change the blueprints for the museum itself, as well as rearrange 
  * the walls, rooms, and exhibits inside the building
  * Created November 26th 2013
- * @author Sara
+ * @author Sara & Jack
  */
 public class FloorPlan {
-	
+
 	Structure[][] floorPlan;
 	
 	/**
@@ -24,6 +24,7 @@ public class FloorPlan {
 	 * Sets the type of the building structure such as outside and inside walls, 
 	 * 	hallways, doors, exhibits, etc
 	 * @param type, representing a type of building structure
+         * @param location , location in the floor plan
 	 */
 	void setType(int[] location, Structure type){
 		this.floorPlan[location[0]][location[1]] = type;
@@ -31,6 +32,7 @@ public class FloorPlan {
 	
 	/** 
 	 * Returns the name of the type of building structure the item is
+         * @param  location, the location in the floor plan
 	 * @return a string representing a type of building structure
 	 */
 	String getType(int[] location){
