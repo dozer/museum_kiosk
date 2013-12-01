2c9927c847a4e6d6d05a4bfc3a3a24b7199a5d11
ABCD6543 <jmcginley4@gmail.com>
2013-12-01 15:57:12 -0500

    Update FloorPlanDAO.java
    Finished FloorPlanDAO, not sure about grid indexes for the Structure[][]

--- MuseumInformationSystem/src/access/FloorPlanDAO.java
+++ MuseumInformationSystem/src/access/FloorPlanDAO.java
@@ -2,6 +2,11 @@ package access;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import enums.Structure;
 import model.FloorPlan;
@@ -10,7 +15,7 @@ import model.FloorPlan;
  * The purpose of this module is to populate FloorPlan objects with their corresponding data 
  * from the database
  * Created November 27th 2013
- * @author Sara
+ * @author Sara, Joe
  *
  */
 public class FloorPlanDAO {
@@ -19,9 +24,30 @@ public class FloorPlanDAO {
 	 * Queries the database to populate a collection of Floor plan objects
 	 * @return a list of Floor Plan objects populated with data
 	 */
-	List<FloorPlan> find(){
-		List<FloorPlan> list = new ArrayList<FloorPlan>();
-		return list;
+	FloorPlan find(){
+		FloorPlan floor;
+  			ResultSet result;
+		int[] index = {0,0};
+                try {
+                        result = sqlQuery("Select * FROM FloorPlan WHERE Enabled = 1");
+                        
+                        while(result.next()){
+                                floor.setType(index, Structure.valueOf(result.getString(2)));
+                                //Not sure what the size of the grid is or if there's a better way to do it.
+                                //Feel free to change.
+                                if(index[0] == 10) {
+                                	index[0] = 0;
+                                	index[1]++;
+                                }
+                                else
+                                	index[0]++;
+                        }        
+                        
+                } catch (ClassNotFoundException | SQLException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+                return floor;
 	}
 	
 	/**
@@ -31,7 +57,60 @@ public class FloorPlanDAO {
 	 */
 	void setType(Structure type){
 		//I changed the parameter from string to structure. feel free to reverse my change
+		try {
+                        sqlUpdate("UPDATE FLOORPLAN SET Structure = '" + type + "'");
+                } catch (ClassNotFoundException | SQLException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
 	}
+	
+	
+        /**
+         * sqlQuery takes a sql statement intended only as a query to retrieve a dataset from the user table
+         * @param sqlStatement
+         * @return resultset of sql query
+         * @throws ClassNotFoundException
+         * @throws SQLException
+         */
+        public static ResultSet sqlQuery(String sqlStatement) throws ClassNotFoundException, SQLException{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/MuseumInformationSystem", "root", "sschlosser3");
+                PreparedStatement statement = con.prepareStatement(sqlStatement);
+                return statement.executeQuery();
+        }
+
+        /**
+         * sqlUpdate takes a sql statement intended only as an update, delete, or insert to the user table
+         * @param sqlStatement
+         * @throws SQLException
+         * @throws ClassNotFoundException
+         */
+        public static void sqlUpdate(String sqlStatement) throws SQLException, ClassNotFoundException{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/MuseumInformationSystem", "root", "sschlosser3");
+                PreparedStatement statement = con.prepareStatement(sqlStatement);
+                statement.executeUpdate();
+        }
+        
+        /**
+         * Method is used as a test case, called from guestView in view folder
+         */
+        public static void floorplantest(){        
+                FloorPlan floor = (FloorPlan) find();
+                while(index[1] != 11) {
+                	System.out.println((floor.getType(index[0], index[1]).toString()));
+                	//Not sure what the size of the grid is or if there's a better way to do it.
+                        //Feel free to change.
+                        if(index[0] == 10) {
+                        	index[0] = 0;
+                                index[1]++;
+                        }
+                        else
+                                index[0]++;
+                }
+        }
+        
 	
 	
 }
