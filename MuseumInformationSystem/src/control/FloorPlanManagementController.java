f6bafe3a7e4b6652fdec6c5483687f0dc748b62d
ABCD6543 <jmcginley4@gmail.com>
2013-12-04 00:44:18 -0500

    Updated FloorPlanManagementController to use database

--- MuseumInformationSystem/src/control/FloorPlanManagementController.java
+++ MuseumInformationSystem/src/control/FloorPlanManagementController.java
@@ -1,76 +1,92 @@
 package control;
 
-import model.FloorPlan;	//might need to be removed
+import model.FloorPlan;
 import enums.Structure;
+import access.FloorPlanDAO;
 
 /**
- * The purpose of the Floor Plan Manager Controller is take the coordinate and object 
- * chosen in Floor Plan Edit View, and to input that object in to that coordinate in the
- * Floor Plan Model.  The Floor Plan Management Controller also manages a list of the current
- * and previous two floor plans.  Floor Plan Management handles falling back to the previous 
- * floor plan designs, as well as adding new FloorPlans to the list, and remove old Floor Plans 
- * from the list
- * Created November 26th 2013
- * @author Sara
+ * The purpose of the Floor Plan Manager Controller is take the coordinate and
+ * object chosen in Floor Plan Edit View, and to input that object in to that
+ * coordinate in the Floor Plan Model. The Floor Plan Management Controller also
+ * manages a list of the current and previous two floor plans. Floor Plan
+ * Management handles falling back to the previous floor plan designs, as well
+ * as adding new FloorPlans to the list, and remove old Floor Plans from the
+ * list Created November 26th 2013
+ *
+ * @author Sara, Joe
  *
  */
 public class FloorPlanManagementController {
 
-	private FloorPlan[] stack = new FloorPlan[3];
-	//undo is added to allocate space for user error in floor plan editing
-	private FloorPlan[] undo = new FloorPlan[3];
+    private FloorPlan[] stack = new FloorPlan[3];
+    //undo is added to allocate space for user error in floor plan editing
+    //Joe's comment: isn't that what the normal stack is for, or am I misinterpreting?
+    private FloorPlan[] undo = new FloorPlan[3];
 
-	/**
-	 * Edits the Floor plan objects which could be the structure type,
-	 * x, or y coordinate.
-	 * @param location representing the x and y coordinate
-	 * @param type representing the structure type of the floor plan
-	 */
-	void editFloorPlan(int[] location, String type){
-		stack[0].setType(location, type);
-	}
+    /**
+     * Edits the Floor plan objects which could be the structure type, x, or y
+     * coordinate.
+     *
+     * @param location representing the x and y coordinate
+     * @param type representing the structure type of the floor plan
+     */
+    void editFloorPlan(int[] location, String type) {
+        FloorPlan fp = stack[0];
+        stack[0].setType(location, type);
+        FloorPlanDAO.updateFloorPlan(stack[0].getFloorPlan(), 0);
+        addToFloorPlanList(fp);
+    }
 
-	/**
-	 * Move takes the object currently in the floor plan in location1
-	 * and moves it to the object in location2.  
-	 * @param location1 representing the old x and y coordinate on the grid
-	 * @param location2 representing the new x and y coordinate on the grid
-	 */
-	void move(int[] location1, int[] location2){
-		String str = stack[0].getType(location2);
-		stack[0].setType(location2, stack[0].getType(location1));
-		stack[0].setType(location1, str);
-	}
+    /**
+     * Move takes the object currently in the floor plan in location1 and moves
+     * it to the object in location2.
+     *
+     * @param location1 representing the old x and y coordinate on the grid
+     * @param location2 representing the new x and y coordinate on the grid
+     */
+    void move(int[] location1, int[] location2) {
+        FloorPlan fp = stack[0];
+        String str = stack[0].getType(location2);
+        stack[0].setType(location2, stack[0].getType(location1));
+        stack[0].setType(location1, str);
+        FloorPlanDAO.updateFloorPlan(stack[0].getFloorPlan(), 0);
+        addToFloorPlanList(fp);
+    }
 
-	/**
-	 * Falls back to the chosen floor plan
-	 * @param fp representing the old floor plan
-	 */
-	void fallbackToFloorPlan(){
-		undo[2] = undo[1];
-		undo[1] = undo[0];
-		undo[0] = stack[0];
-		stack[0] = stack[1];
-		stack[1] = stack[2];
-		stack[2] = null;
+    /**
+     * Falls back to the previous floor plan
+     *
+     */
+    void fallbackToFloorPlan() {
+        //Joe's comment: as stated above, not sure of the use of the undo stack
+        //undo[2] = undo[1];
+        //undo[1] = undo[0];
+        //undo[0] = stack[0];
+        
+        stack[0] = stack[1];
+        FloorPlanDAO.updateFloorPlan(stack[1].getFloorPlan(), 0);
+        stack[1] = stack[2];
+        FloorPlanDAO.updateFloorPlan(stack[2].getFloorPlan(), 1);
+        stack[2] = null;
 
-	}
+    }
 
-	/**
-	 * Adds the new Floor Plan created by editFloorPlan to the list of FloorPlans
-	 * @param fp representing a new floorplan
-	 */
-	void addToFloorPlanList(FloorPlan fp){
-		stack[2] = stack[1];
-		stack[1] = stack[0];
-		stack[0] = fp;
-	}
+    /**
+     * Moves the queue back for the old floor plans
+     *
+     * @param fp representing the previous floor plan
+     */
+    void addToFloorPlanList(FloorPlan fp) {
+        stack[2] = stack[1];
+        FloorPlanDAO.updateFloorPlan(stack[1].getFloorPlan(), 2);
+        stack[1] = fp;
+        FloorPlanDAO.updateFloorPlan(fp.getFloorPlan(), 1);
+    }
 
-	/**
-	 * Removes last floor plan from the floor plan list.
-	 */
-	void removeFromFloorPlanList(){
-		//not sure if neccessary
-
-	}
+    /**
+     * Removes last floor plan from the floor plan list.
+     */
+    void removeFromFloorPlanList() {
+        //not sure if neccessary
+    }
 }
