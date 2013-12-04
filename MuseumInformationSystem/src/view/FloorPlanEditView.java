c964e369325db8fd80aaa74851b72e53b0f891f9
Casey Caprice <caseytcaprice@gmail.com>
2013-12-03 23:46:57 -0500

    Added comments to FloorPlanEditView

--- MuseumInformationSystem/src/view/FloorPlanEditView.java
+++ MuseumInformationSystem/src/view/FloorPlanEditView.java
@@ -19,7 +19,7 @@ import javax.swing.JPanel;
  * @author Sara
  */
 public class FloorPlanEditView extends JFrame {
-	private static final int N = 18;
+	private static final int N = 18;				
     private static final int SIZE = 60;
     static Object[][] gridObject = new Object[N][N];
     static String currentSelectedObject = "Wall";
@@ -30,6 +30,11 @@ public class FloorPlanEditView extends JFrame {
     JButton item;
     JButton space;
     
+    /**
+     * Create the Panel, as well as all coordinates and object buttons.  The User will
+     * be able to click a button to choose which type of object (Wall, Exhibit, Item, Open Space)
+     * to populate coordinates within the Floor Plan.
+     */
     public FloorPlanEditView() {
     	currentObject = new Object("Wall", -1, -1, "", "", "");
     	
@@ -40,6 +45,158 @@ public class FloorPlanEditView extends JFrame {
     	
     	JPanel bottomGrid = new JPanel(new GridLayout(4,1));
     	
+    	createButtons();
+    	
+    	bottomGrid.add(wall);
+    	bottomGrid.add(exhibit);
+    	bottomGrid.add(item);
+    	bottomGrid.add(space);
+    	
+    	GridBagLayout gridBag = new GridBagLayout();
+    	
+    	JPanel mainPanel = new JPanel(gridBag);
+    	
+    	mainPanel.add(coordGrid);
+    	mainPanel.add(bottomGrid);
+    	this.add(mainPanel);
+    }
+    
+    /**
+     * Generic class to create buttons that will be used as coordinates.  When a
+     * button is clicked, it will take on the properties of the selected object
+     * @author caseytcaprice
+     *
+     */
+    private static class Button extends JButton {
+    	int x;
+    	int y;
+    	
+    	/**
+    	 * Turns button green
+    	 */
+    	void makeGreen()
+    	{
+    		this.setBackground(Color.green);
+    	}
+    	
+    	/**
+    	 * Turns button red
+    	 */
+    	void makeRed()
+    	{
+    		this.setBackground(Color.red);
+    	}
+    	
+    	/**
+    	 * Turns button blue
+    	 */
+    	void makeBlue()
+    	{
+    		this.setBackground(Color.blue);
+    	}
+    	
+    	/**
+    	 * Turns button into open space
+    	 */
+    	void makeSpace()
+    	{
+    		this.setBackground(Color.white);
+    	}
+    	
+    	/**
+    	 * Button Constructor, creates a generic white button with a coordinate.
+    	 * @param i, representing the number of Button created.  Used for coordinates
+    	 */
+        public Button(int i) {
+            super(i / N + "," + i % N);
+            this.setOpaque(true);
+            this.setBorderPainted(true);
+            this.setBackground(Color.white);
+                        
+            this.x = i / N;
+            this.y = i % N;
+            
+            this.addActionListener(new ActionListener() {
+            	public void actionPerformed(ActionEvent e)
+            	{
+            		currentObject.setX(x);
+            		currentObject.setY(y);
+            		
+            		gridObject[x][y] = currentObject;
+            		
+            		switch(currentSelectedObject)
+            		{
+            			case("Wall"):
+            				makeRed();
+            				break;
+            			case("Exhibit"):
+            				makeGreen();
+            				break;
+            			case("Item"):
+            				makeBlue();
+            				break;
+            			case("Space"):
+            				makeSpace();
+            			default:
+            				break;            			
+            		}
+            	}
+            });            
+        }
+    }
+
+    /**
+     * Displays Panel
+     */
+    private void display() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    /**
+     * Set current coordinate to an object type, Wall, Exhibit, Item, or Open Space.
+     * @param type Object type, Wall, Exhibit, Item, Open Space
+     */
+    private void setCurrent(String type){
+    	if(type == "Wall") {
+    		currentSelectedObject = "Wall";
+    		wall.setFont(space.getFont().deriveFont(Font.BOLD | Font.ITALIC));
+    	}
+    	else {
+    		wall.setFont(wall.getFont().deriveFont(Font.PLAIN));
+    	}
+    	
+    	if(type == "Exhibit") {
+    		currentSelectedObject = "Exhibit";
+    		exhibit.setFont(space.getFont().deriveFont(Font.BOLD | Font.ITALIC));
+    	}
+    	else {
+    		exhibit.setFont(wall.getFont().deriveFont(Font.PLAIN));
+    	}
+    	
+    	if(type == "Item") {
+    		currentSelectedObject = "Item";
+    		item.setFont(space.getFont().deriveFont(Font.BOLD | Font.ITALIC));
+    	}
+    	else {
+    		item.setFont(wall.getFont().deriveFont(Font.PLAIN));
+    	}
+    	
+    	if(type == "Space") {
+    		currentSelectedObject = "Space";
+    		space.setFont(space.getFont().deriveFont(Font.BOLD | Font.ITALIC));
+    	}
+    	else {
+    		space.setFont(wall.getFont().deriveFont(Font.PLAIN));
+    	}    	
+    }
+    
+    /**
+     * Create Wall, Exhibit, Item, and Open Space buttons
+     */
+    public void createButtons() {
     	wall = new JButton();
     	wall.setText("Wall");
     	wall.setForeground(Color.red);
@@ -91,124 +248,10 @@ public class FloorPlanEditView extends JFrame {
         		setCurrent("Space");
         	}
         });
-    	
-    	bottomGrid.add(wall);
-    	bottomGrid.add(exhibit);
-    	bottomGrid.add(item);
-    	bottomGrid.add(space);
-    	
-    	GridBagLayout gridBag = new GridBagLayout();
-    	
-    	JPanel mainPanel = new JPanel(gridBag);
-    	
-    	mainPanel.add(coordGrid);
-    	mainPanel.add(bottomGrid);
-    	this.add(mainPanel);
     }
     
-    private static class Button extends JButton {
-    	int x;
-    	int y;
-    	void makeGreen()
-    	{
-    		this.setBackground(Color.green);
-    	}
-    	
-    	void makeRed()
-    	{
-    		this.setBackground(Color.red);
-    	}
-    	
-    	void makeBlue()
-    	{
-    		this.setBackground(Color.blue);
-    	}
-    	
-    	void makeSpace()
-    	{
-    		this.setBackground(Color.white);
-    	}
-    	
-        public Button(int i) {
-            super(i / N + "," + i % N);
-            this.setOpaque(true);
-            this.setBorderPainted(true);
-            this.setBackground(Color.white);
-                        
-            this.x = i / N;
-            this.y = i % N;
-            
-            this.addActionListener(new ActionListener() {
-            	public void actionPerformed(ActionEvent e)
-            	{
-            		currentObject.setX(x);
-            		currentObject.setY(y);
-            		
-            		gridObject[x][y] = currentObject;
-            		
-            		switch(currentSelectedObject)
-            		{
-            			case("Wall"):
-            				makeRed();
-            				break;
-            			case("Exhibit"):
-            				makeGreen();
-            				break;
-            			case("Item"):
-            				makeBlue();
-            				break;
-            			case("Space"):
-            				makeSpace();
-            			default:
-            				break;            			
-            		}
-            	}
-            });            
-        }
-    }
-
-    private void display() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-    }
-
-    private void setCurrent(String type){
-    	if(type == "Wall") {
-    		currentSelectedObject = "Wall";
-    		wall.setFont(space.getFont().deriveFont(Font.BOLD | Font.ITALIC));
-    	}
-    	else {
-    		wall.setFont(wall.getFont().deriveFont(Font.PLAIN));
-    	}
-    	
-    	if(type == "Exhibit") {
-    		currentSelectedObject = "Exhibit";
-    		exhibit.setFont(space.getFont().deriveFont(Font.BOLD | Font.ITALIC));
-    	}
-    	else {
-    		exhibit.setFont(wall.getFont().deriveFont(Font.PLAIN));
-    	}
-    	
-    	if(type == "Item") {
-    		currentSelectedObject = "Item";
-    		item.setFont(space.getFont().deriveFont(Font.BOLD | Font.ITALIC));
-    	}
-    	else {
-    		item.setFont(wall.getFont().deriveFont(Font.PLAIN));
-    	}
-    	
-    	if(type == "Space") {
-    		currentSelectedObject = "Space";
-    		space.setFont(space.getFont().deriveFont(Font.BOLD | Font.ITALIC));
-    	}
-    	else {
-    		space.setFont(wall.getFont().deriveFont(Font.PLAIN));
-    	}    	
-    }
     public static void main(String[] args) {
-       /* EventQueue.invokeLater(new Runnable() {
+        /*EventQueue.invokeLater(new Runnable() {
 
             @Override
             public void run() {
@@ -217,6 +260,12 @@ public class FloorPlanEditView extends JFrame {
         });*/
     }
     
+    /**
+     * An Object that will be contained within an array.  Will hold all information about what is
+     * held within coordinate
+     * @author caseytcaprice
+     *
+     */
     private class Object {
     	private String type;
     	private int x;
