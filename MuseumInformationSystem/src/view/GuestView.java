c207f604d430c1d324a78178b6182a9652df7d93
webste32 <webste32@students.rowan.edu>
2013-12-03 22:57:18 -0500

    fixed URL to point to MCH html files

--- MuseumInformationSystem/src/view/GuestView.java
+++ MuseumInformationSystem/src/view/GuestView.java
@@ -2,6 +2,9 @@ package view;
 
 import java.beans.Statement;
 
+
+import java.net.URL;
+
 import access.ExhibitDAO;
 import access.MuseumItemDAO;
 import access.UserDAO;
@@ -35,19 +38,14 @@ public class GuestView extends Application {
     private Scene scene;
     @Override public void start(Stage stage) {
         // create the scene
-        stage.setTitle("Web View");
+        stage.setTitle("Museum Information System");
         scene = new Scene(new Browser(),850,600, Color.web("#666970"));
-        stage.setScene(scene);
-        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");        
+        stage.setScene(scene);     
         stage.show();
     }
  
     public static void main(String[] args) {
-        //launch(args);
-    	//UserDAO.usertest();
-    	//MuseumItemDAO.museumtest();
-    	ExhibitDAO.exhibittest();
-
+        launch(args);
     }
     
 
@@ -73,6 +71,7 @@ public class GuestView extends Application {
     }
     
 }
+
 class Browser extends Region {
  
     final WebView browser = new WebView();
@@ -82,7 +81,11 @@ class Browser extends Region {
         //apply the styles
         getStyleClass().add("browser");
         // load the web page
-        webEngine.load("http://www.google.com");
+        
+        
+        URL url = getClass().getResource("../MCH_html/index.html");
+        webEngine.load(url.toExternalForm());
+        
         //add the web view to the scene
         getChildren().add(browser);
  
